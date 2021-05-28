9351ad944951205314a6c6fd7e4e75617a32ad1b
Olga Rozhkova <olga.gavrilova.og94@gmail.com>
2021-05-28 19:38:16 +0300

    add hamburger-menu

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const benefitsHidden = document.querySelector('.hidden'),
-    moreInfo = document.querySelector('.more_info');
+    moreInfo = document.querySelector('.more_info'),
+    hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    menuLink= document.querySelectorAll('.menu_link');
 
 moreInfo.addEventListener('click', () => { 
-    benefitsHidden.classList.remove('hidden');
-    moreInfo.remove();
+    benefitsHidden.classList.toggle('hidden');
 });
-
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('active');
+});
+menuLink.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('active');
+    })
+})
+ 
